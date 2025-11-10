ff5746a8dd565bcbf996964cb7d5e5d63b57fb08
Aditya Bhujade <bhujadeaditya28@gmail.com>
2025-11-10 22:32:20 +0530

    optimized models again

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -36,27 +36,32 @@ class ComputerErrorBoundary extends React.Component {
 }
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+  try {
+    const computer = useGLTF('./desktop_pc/scene.gltf')
 
-  return (
-    <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <primitive
-        object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
-    </mesh>
-  )
+    return (
+      <mesh>
+        <hemisphereLight intensity={0.15} groundColor='black' />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        <primitive
+          object={computer.scene}
+          scale={isMobile ? 0.7 : 0.75}
+          position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
+      </mesh>
+    )
+  } catch (error) {
+    console.error('Error loading computer model:', error);
+    return null;
+  }
 }
 
 // Preload the model
@@ -67,7 +72,7 @@ const ComputersCanvas = () => {
 
   useEffect(() => {
     // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia('(max-width: 500px)')
+    const mediaQuery = window.matchMedia('(max-width: 768px)')
 
     // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches)
@@ -90,13 +95,14 @@ const ComputersCanvas = () => {
     <ComputerErrorBoundary>
       <Canvas
         frameloop='always'
-        shadows
-        dpr={[1, 2]}
+        shadows={!isMobile}
+        dpr={isMobile ? [1, 1] : [1, 2]}
         camera={{ position: [20, 3, 5], fov: 25 }}
         gl={{ 
           preserveDrawingBuffer: true,
           powerPreference: 'default',
-          antialias: true
+          antialias: !isMobile,
+          alpha: false
         }}
       >
         <Suspense fallback={<CanvasLoader />}>
